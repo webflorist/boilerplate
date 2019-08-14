2c8cfa322bcf91d596899f17a229c89e8ea3449e
Gerald Buttinger <gerald@code.florist>
2019-08-14 19:50:30 +0200

    add some translations

--- src/resources/lang/de/forms.php
+++ src/resources/lang/de/forms.php
@@ -4,4 +4,5 @@ return [
     'select_file' => 'Datei wählen',
     'change_file' => 'Ändern',
     'remove_file' => 'Löschen',
+    'contact_data' => 'Kontaktdaten',
 ];
--- src/resources/lang/en/forms.php
+++ src/resources/lang/en/forms.php
@@ -4,4 +4,5 @@ return [
     'select_file' => 'Select file',
     'change_file' => 'Change',
     'remove_file' => 'Remove',
+    'contact_data' => 'Contact Data',
 ];
