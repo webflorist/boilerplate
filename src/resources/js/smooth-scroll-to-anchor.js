ca23fa48f9cb1e8553955983e834e5c48580ec75
Gerald Buttinger <gerald@code.florist>
2019-08-17 00:03:32 +0200

    depend smooth scroll to anchor on class 'smooth-scroll'

--- src/resources/js/smooth-scroll-to-anchor.js
+++ src/resources/js/smooth-scroll-to-anchor.js
@@ -1,9 +1,9 @@
-$(document).on('click', 'a[href^="#"]', function (event) {
+$(document).on('click', 'a.smooth-scroll[href^="#"]', function (event) {
     let href = $.attr(this, 'href');
     if (href !== '#') {
         event.preventDefault();
         $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
+            scrollTop: $($.attr(this, 'href')).offset().top -100
         }, 500);
     }
 });
