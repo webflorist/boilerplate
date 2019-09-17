17fee04b9b45125418beb07f921d3e54939839ca
Gerald Buttinger <gerald@code.florist>
2019-09-17 22:52:03 +0200

    make parallax header optional in master template

--- src/resources/views/paper-kit-2/_partials/header.blade.php
+++ src/resources/views/paper-kit-2/_partials/header.blade.php
@@ -1,5 +1,6 @@
 @if($showHeader ?? true)
-    <header class="page-header @isset($headerSize) page-header-{{$headerSize}} @endisset" data-parallax="true"
+    <header class="page-header @isset($headerSize) page-header-{{$headerSize}} @endisset"
+            @if($parallaxHeader ?? false) data-parallax="true" @endif
             style="background-image: url('{{$headerImage}}');">
         {{$headerContent}}
     </header>
