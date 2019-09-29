33b399122c87fcd6f9b72a2d3a61ee0906e111be
Gerald Buttinger <gerald@code.florist>
2019-09-30 00:28:31 +0200

    add smooth scrolling to anchor on page load

--- src/resources/js/smooth-scroll-to-anchor.js
+++ src/resources/js/smooth-scroll-to-anchor.js
@@ -1,9 +1,27 @@
+/* On clicking an anchor-link with class 'smooth-scroll' */
 $(document).on('click', 'a.smooth-scroll[href^="#"]', function (event) {
     let href = $.attr(this, 'href');
     if (href !== '#') {
         event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top -100
-        }, 500);
+        smooth_scroll_to_anchor($.attr(this, 'href'));
     }
 });
+
+/* On page-load with anchor in url */
+// prevent default scrolling
+if (window.location.hash) scroll(0, 0);
+setTimeout(function () {
+    scroll(0, 0);
+}, 1);
+$(document).ready(function () {
+
+    if (window.location.hash) {
+        smooth_scroll_to_anchor(window.location.hash);
+    }
+});
+
+function smooth_scroll_to_anchor(anchor) {
+    $('html, body').animate({
+        scrollTop: $(anchor).offset().top - 100
+    }, 500, 'swing');
+}
