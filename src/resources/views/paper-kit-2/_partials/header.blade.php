17643e4dde72a5c1e79a991b02b0839919b19fc8
Gerald Buttinger <gerald@code.florist>
2019-10-03 00:46:16 +0200

    some template improvements

--- src/resources/views/paper-kit-2/_partials/header.blade.php
+++ src/resources/views/paper-kit-2/_partials/header.blade.php
@@ -1,7 +1,7 @@
 @if($showHeader ?? true)
-    <header class="page-header @isset($headerSize) page-header-{{$headerSize}} @endisset"
+    <header class="page-header @isset($headerSize)page-header-{{$headerSize}} @endisset"
             @if($parallaxHeader ?? false) data-parallax="true" @endif
-            style="background-image: url('{{$headerImage}}');">
+            style="background-image: url('{{mix($headerImage)}}');">
         {{$headerContent}}
     </header>
 @endif
