6b332681e3c2cb1e2a39ce12be69e96d1a228074
Gerald Buttinger <gerald@code.florist>
2019-10-04 15:11:34 +0200

    add google tag manager script and configurable id

--- src/config/boilerplate.php
+++ src/config/boilerplate.php
@@ -11,6 +11,16 @@ return [
     | It gets linked in the cookie consent notification.
     |
     */
-    'privacy_policy_node' => 'privacy-policy'
+    'privacy_policy_node' => 'privacy-policy',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Google Tag Manager ID (aka Container ID)
+    |--------------------------------------------------------------------------
+    |
+    | Set the Google Tag Manager ID.
+    |
+    */
+    'google_tag_manager_id' => env('GOOGLE_TAG_MANAGER_ID', null)
 
 ];
--- src/resources/views/_general/master.blade.php
+++ src/resources/views/_general/master.blade.php
@@ -1,6 +1,19 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+
+
+    @if(!is_null(config('boilerplate.google_tag_manager_id')))
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','{{config('boilerplate.google_tag_manager_id')}}');
+        </script>
+        <!-- End Google Tag Manager -->
+    @endisset
+
     @php($routeNode = route_tree()->getCurrentNode())
     <title>{{$documentTitle}}</title>
     <meta name="description" content="{{$routeNode->getData('metaDescription')}}">
@@ -33,6 +46,14 @@
 </head>
 
 <body class="{{$routeNode->getData('bodyClass')}}">
+
+@if(!is_null(config('boilerplate.google_tag_manager_id')))
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id={{config('boilerplate.google_tag_manager_id')}}" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+@endif
 
 {{$body}}
 
