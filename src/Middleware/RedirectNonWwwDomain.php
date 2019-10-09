a924730f6a6d6f24d0bbe3be0ca24b2657152e5e
Gerald Buttinger <gerald@code.florist>
2019-10-09 17:33:34 +0200

    add log

--- src/Middleware/RedirectNonWwwDomain.php
+++ src/Middleware/RedirectNonWwwDomain.php
@@ -3,6 +3,7 @@
 namespace Webflorist\Boilerplate\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class RedirectNonWwwDomain
 {
@@ -16,8 +17,10 @@ class RedirectNonWwwDomain
     public function handle($request, Closure $next)
     {
         $host = $request->getHttpHost();
+        Log::info("RedirectNonWwwDomain: $host");
         if (substr($host, 0, 4) != 'www.') {
             $request->headers->set('host', 'www.'.$host);
+            Log::info("Redirecting to: ".$request->fullUrl());
             return redirect($request->fullUrl(), 301);
         }
         return $next($request);
