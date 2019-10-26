323970af19864c34a452aa5b13a934f8f155f2bf
Gerald Buttinger <gerald@code.florist>
2019-10-27 01:01:53 +0200

    add some translations

--- src/resources/lang/en/forms.php
+++ src/resources/lang/en/forms.php
@@ -7,5 +7,8 @@ return [
     'download_file' => 'Download file',
     'contact_data' => 'Contact Data',
     'terms' => 'Terms',
-    'accept_terms' => 'I accept the <a href="' . route_node_url('terms') . '" target="_blank">terms and conditions</a>.'
+    'accept_terms' => 'I accept the <a href="' . route_node_url('terms') . '" target="_blank">terms and conditions</a>.',
+    'drop_or_click_to_upload' => 'Drop or select file(s)',
+    'invalid_file_type' => 'File type must be :allowed.',
+    'invalid_file_size' => 'File size must not be larger than :max.'
 ];
