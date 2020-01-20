cb5add3be94383f61939e41fc91244b5f4ccc918
Gerald Buttinger <gerald@code.florist>
2020-01-20 21:29:11 +0100

    fix problem with cookie-accept and no tracking-id

--- src/resources/views/_general/master.blade.php
+++ src/resources/views/_general/master.blade.php
@@ -23,9 +23,9 @@
         <!-- End Google Tag Manager -->
     @endisset
 
-    @if(!is_null(config('boilerplate.google_analytics_tracking_id')))
-        <script>
-            function initGoogleAnalytics(){
+    <script>
+        function initGoogleAnalytics(){
+            @if(!is_null(config('boilerplate.google_analytics_tracking_id')))
                 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
                 ga('create', '{{config('boilerplate.google_analytics_tracking_id')}}', 'auto');
                 ga('set', 'anonymizeIp', true);
@@ -34,9 +34,9 @@
                 gascript.async = true;
                 gascript.src = "https://www.google-analytics.com/analytics.js";
                 document.getElementsByTagName("head")[0].appendChild(gascript, document.getElementsByTagName("head")[0]);
-            }
-        </script>
-    @endisset
+            @endisset
+        }
+    </script>
 
     @php($routeNode = route_tree()->getCurrentNode())
     <title>{{$documentTitle}}</title>
