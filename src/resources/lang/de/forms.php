77c589947bb1d8d94f9920733655a2ceb9cfda5d
Gerald Buttinger <gerald@code.florist>
2020-01-20 21:14:51 +0100

    add forms.accept_privacy_policy translations

--- src/resources/lang/de/forms.php
+++ src/resources/lang/de/forms.php
@@ -8,6 +8,7 @@ return [
     'contact_data' => 'Kontaktdaten',
     'terms' => 'Geschäftsbedingungen',
     'accept_terms' => 'Ich akzeptiere die <a href="' . route_node_url('terms') . '" target="_blank">Allgemeinen Geschäftsbedingungen</a> sowie die <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">Datenschutzerklärung</a>.',
+    'accept_privacy_policy' => 'Ich akzeptiere die <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">Datenschutzerklärung</a>.',
     'drop_or_click_to_upload' => 'Datei(en) ablegen oder auswählen',
     'invalid_file_type' => 'Datei muss von Typ :allowed sein.',
     'invalid_file_size' => 'Dateigröße darf :max nicht überschreiten.'
--- src/resources/lang/en/forms.php
+++ src/resources/lang/en/forms.php
@@ -8,6 +8,7 @@ return [
     'contact_data' => 'Contact Data',
     'terms' => 'Terms',
     'accept_terms' => 'I accept the <a href="' . route_node_url('terms') . '" target="_blank">terms and conditions</a> as well as the <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">privacy policy</a>.',
+    'accept_privacy_policy' => 'I accept the <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">privacy policy</a>.',
     'drop_or_click_to_upload' => 'Drop or select file(s)',
     'invalid_file_type' => 'File type must be :allowed.',
     'invalid_file_size' => 'File size must not be larger than :max.'
