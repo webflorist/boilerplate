397bfcda0f356924ce75937a4b8e20774e9f9602
Gerald Buttinger <gerald@code.florist>
2020-01-27 20:23:12 +0100

    fix language file

--- src/resources/lang/de/forms.php
+++ src/resources/lang/de/forms.php
@@ -7,7 +7,7 @@ return [
     'download_file' => 'Datei herunterladen',
     'contact_data' => 'Kontaktdaten',
     'terms' => 'Geschäftsbedingungen',
-    'accept_terms' => 'Ich akzeptiere die <a href="' . route_node_url('terms') . '" target="_blank">Allgemeinen Geschäftsbedingungen</a> sowie die <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">Datenschutzerklärung</a>.',
+    'accept_terms' => 'Ich akzeptiere die <a href="' . (route_tree()->doesNodeExist('terms') ? route_node('terms')->getUrl() : '') . '" target="_blank">Allgemeinen Geschäftsbedingungen</a> sowie die <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">Datenschutzerklärung</a>.',
     'accept_privacy_policy' => 'Ich akzeptiere die <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">Datenschutzerklärung</a>.',
     'drop_or_click_to_upload' => 'Datei(en) ablegen oder auswählen',
     'invalid_file_type' => 'Datei muss von Typ :allowed sein.',
--- src/resources/lang/en/forms.php
+++ src/resources/lang/en/forms.php
@@ -7,7 +7,7 @@ return [
     'download_file' => 'Download file',
     'contact_data' => 'Contact Data',
     'terms' => 'Terms',
-    'accept_terms' => 'I accept the <a href="' . route_node_url('terms') . '" target="_blank">terms and conditions</a> as well as the <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">privacy policy</a>.',
+    'accept_terms' => 'I accept the <a href="' . (route_tree()->doesNodeExist('terms') ? route_node('terms')->getUrl() : '') . '" target="_blank">terms and conditions</a> as well as the <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">privacy policy</a>.',
     'accept_privacy_policy' => 'I accept the <a href="' . route_node_url(config('boilerplate.privacy_policy_node')) . '" target="_blank">privacy policy</a>.',
     'drop_or_click_to_upload' => 'Drop or select file(s)',
     'invalid_file_type' => 'File type must be :allowed.',
