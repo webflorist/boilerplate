b27e2cd8d59955d10faf55844964b3a99ed6d349
Gerald Buttinger <gerald@code.florist>
2020-03-30 17:34:07 +0200

    change smooth-scroll-to-anchor.js to use native JS

--- src/resources/js/smooth-scroll-to-anchor.js
+++ src/resources/js/smooth-scroll-to-anchor.js
@@ -1,27 +1,10 @@
 /* On clicking an anchor-link with class 'smooth-scroll' */
-$(document).on('click', 'a.smooth-scroll[href^="#"]', function (event) {
-    let href = $.attr(this, 'href');
-    if (href !== '#') {
-        event.preventDefault();
-        smooth_scroll_to_anchor($.attr(this, 'href'));
-    }
+document.querySelectorAll('a.smooth-scroll[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
-
-/* On page-load with anchor in url */
-// prevent default scrolling
-if (window.location.hash) scroll(0, 0);
-setTimeout(function () {
-    scroll(0, 0);
-}, 1);
-$(document).ready(function () {
-
-    if (window.location.hash) {
-        smooth_scroll_to_anchor(window.location.hash);
-    }
-});
-
-function smooth_scroll_to_anchor(anchor) {
-    $('html, body').animate({
-        scrollTop: $(anchor).offset().top - 100
-    }, 500, 'swing');
-}
