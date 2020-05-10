9b29147ab1bdd967fcb4a5f7598492b2e3e9ad47
Gerald Buttinger <gerald@code.florist>
2020-05-11 00:31:03 +0200

    smooth scroll to anchor on page load

--- src/resources/js/smooth-scroll-to-anchor.js
+++ src/resources/js/smooth-scroll-to-anchor.js
@@ -9,3 +9,21 @@ document.querySelectorAll('a.smooth-scroll[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+/* On loading a page with an anchor in it's URL */
+// to top right away
+if (window.location.hash) scroll(0, 0);
+// void some browsers issue
+setTimeout(function () {
+    scroll(0, 0);
+}, 1);
+$(function () {
+    // *only* if we have anchor on the url
+    if (window.location.hash) {
+        document.querySelector(window.location.hash).scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    }
+
+});
