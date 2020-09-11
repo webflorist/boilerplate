24e4e637a3752c3c561e08edbb1cd2b279a4875c
Gerald Buttinger <gerald@code.florist>
2020-09-11 17:38:06 +0200

    make googlemap component dual-language

--- src/resources/js/components/GoogleMap.vue
+++ src/resources/js/components/GoogleMap.vue
@@ -4,7 +4,7 @@
             <div class="w-5 mr-3">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marked-alt" class="svg-inline--fa fa-map-marked-alt fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M288 0c-69.59 0-126 56.41-126 126 0 56.26 82.35 158.8 113.9 196.02 6.39 7.54 17.82 7.54 24.2 0C331.65 284.8 414 182.26 414 126 414 56.41 357.59 0 288 0zm0 168c-23.2 0-42-18.8-42-42s18.8-42 42-42 42 18.8 42 42-18.8 42-42 42zM20.12 215.95A32.006 32.006 0 0 0 0 245.66v250.32c0 11.32 11.43 19.06 21.94 14.86L160 448V214.92c-8.84-15.98-16.07-31.54-21.25-46.42L20.12 215.95zM288 359.67c-14.07 0-27.38-6.18-36.51-16.96-19.66-23.2-40.57-49.62-59.49-76.72v182l192 64V266c-18.92 27.09-39.82 53.52-59.49 76.72-9.13 10.77-22.44 16.95-36.51 16.95zm266.06-198.51L416 224v288l139.88-55.95A31.996 31.996 0 0 0 576 426.34V176.02c0-11.32-11.43-19.06-21.94-14.86z"></path></svg>
             </div>
-            Karte {{ show ? 'ausblenden' : 'einblenden' }}
+            {{ show ? trans('hide_map') : trans('show_map') }}
             <div class="w-3 ml-3">
                 <svg v-if="show" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-up" class="svg-inline--fa fa-angle-up fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M177 159.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 255.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 329.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1z"></path></svg>
                 <svg v-if="!show" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-down" class="svg-inline--fa fa-angle-down fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"></path></svg>
@@ -24,28 +24,46 @@
         props: {
             src: String,
             buttonClass: String,
-            iframeClass: String
+            iframeClass: String,
+            locale: String
         },
         mounted() {
             this.show = false;
         },
         data: function () {
             return {
-                show: false
+                show: false,
+                lang: {
+                    de: {
+                        show_map: "Karte einblenden",
+                        hide_map: "Karte ausblenden",
+                        html: '<p>Zur Darstellung der Karte wird der Dienst "Google Maps" des Dritt-Anbieters "Google Inc." verwendet.<br>Ist das OK für Sie?</p><hr class="my-3"><p class="small text-sm">Wenn Sie zustimmen, werden Google Maps bezogene Cookies gesetzt und Datenverarbeitungen vorgenommen.</p><p class="small text-sm">Mehr Informationen finden Sie in der <a href="/datenschutz" target="_blank">Datenschutzerklärung</a>! Dort können Sie Ihre Auswahl auch jederzeit widerrufen.</p>',
+                        cancel_button: 'Abbrechen (Karte nicht anzeigen)'
+                    },
+                    en: {
+                        show_map: "Show map",
+                        hide_map: "Hide map",
+                        html: '<p>The map uses the service "Google Maps" by the third party service provider "Google Inc.".<br>Is this OK for you?</p><hr class="my-3"><p class="small text-sm">If you accept, Google Maps related cookies and data processings will be used.</p><p class="small text-sm">More information can be found in our <a href="/privacy" target="_blank">Privacy Policy</a>! There you can withdraw your consent anytime.</p>',
+                        cancel_button: 'Cancel (do not display map)'
+                    }
+                }
             }
         },
         methods: {
+            trans: function (key) {
+                return this.lang[this.locale][key];
+            },
             toggle: function () {
                 if (!this.show && !this.isGoogleMapsAccepted()) {
                     return Swal.fire({
                         title: 'Google Maps',
-                        html: '<p>Zur Darstellung der Karte wird der Dienst "Google Maps" des Dritt-Anbieters "Google Inc." verwendet.<br>Ist das OK für Sie?</p><hr class="my-3"><p class="small text-sm">Wenn Sie zustimmen, werden Google Maps bezogene Cookies gesetzt und Datenverarbeitungen vorgenommen.</p><p class="small text-sm">Mehr Informationen finden Sie in der <a href="/datenschutz" target="_blank">Datenschutzerklärung</a>! Dort können Sie Ihre Auswahl auch jederzeit widerrufen.</p>',
+                        html: this.trans('html'),
                         icon: "question",
                         showCancelButton: true,
                         confirmButtonColor: "#28a745",
                         cancelButtonColor: "#616161",
                         confirmButtonText: "OK",
-                        cancelButtonText: "Abbrechen (Karte nicht anzeigen)"
+                        cancelButtonText: this.trans('cancel_button')
                     }).then((result) => {
                         if(result.value) {
                             this.setCookie('acceptGoogleMaps', true, 365);
